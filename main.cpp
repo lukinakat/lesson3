0e06f3fc23f757813be0249d68148b6626012a83
ekaterina <lukina.kat@mail.ru>
2020-11-08 22:31:55 +0300

    Task for lesson 3

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int result = 0;
+    for(int i = 5; i < 25; i++)
+    {
+        if (i % 2 == 1)
+        {
+            result += i;
+        }
+    }
+    cin << result;
+}
