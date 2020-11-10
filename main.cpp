7bbcdecdbb13f1d311487fd1e42b26be30275907
Kirill <lvovkir@yandex.ru>
2020-11-10 23:51:41 +0300

    Исправили ошибки в main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@ using namespace std;
 int main()
 {
     int result = 0;
-    for(int i = 5; i < 25; i++)
-    {
-        if (i % 2 == 1)
-        {
+    for(int i = 5; i < 25; i++){
+        if (i % 2 == 0)
             result += i;
-        }
     }
-    cin << result;
+    cout << result;
 }
