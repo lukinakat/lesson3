b5b500a10d5192b92497d5e95ed38b2f3f583053
Николай Гришанов <nikolasgrishanov@gmail.com>
2020-11-22 23:25:56 +0300

    fixed

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
     int result = 0;
-    for(int i = 5; i < 25; i++)
+    for(int i = 6; i <= 24; i++)
     {
-        if (i % 2 == 1)
+        if (i % 2 == 0)
         {
             result += i;
         }
     }
-    cin << result;
+    cout << result;
 }
