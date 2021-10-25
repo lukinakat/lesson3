8d0ad85fd0282d2715b65e55994832e5c9153ec9
vasiliy odinokov <10p2_11@intra.l1523.ru>
2021-10-25 14:05:35 +0300

    Edit1

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-int main()
+int main() 
 {
     int result = 0;
-    for(int i = 5; i < 25; i++)
+    for(int i = 5; i <= 25; i++)
     {
-        if (i % 2 == 1)
-        {
-            result += i;
-        }
+        result += i;
     }
-    cin << result;
+    cout<<result;
 }
