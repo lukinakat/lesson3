a31bff75d99a86d6f38a7bb822dd8acf96e73103
vasiliy odinokov <10p2_11@intra.l1523.ru>
2021-10-25 14:11:58 +0300

    bAgfix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-int main() 
+int main()
 {
     int result = 0;
-    for(int i = 5; i <= 25; i++)
+    for(int i = 5;i <= 25;i++)
     {
         result += i;
     }
